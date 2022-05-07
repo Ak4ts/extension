a35f82dcec6daf88c0e6f1afab0adab59f8f81f8
Luiz Guilherme de Melo <luiz.melo@alunos.ifsuldeminas.edu.br>
2022-05-06 22:04:09 -0300

    .

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-document.addEventListener("selectionchange", () => {
-  console.log(document.getSelection().focusNode.data)
-})
+var textConfig = window.getSelection();
+var text = ""
 
-console.log(tr("hi"))
+document.addEventListener("mouseup", () => {
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "POST",
+    "https://translate-extension-test.herokuapp.com/translate",
+    true
+  );
+  xhr.setRequestHeader("Content-Type", "application/json"); //Obrigatorio API
+
+  xhr.onload = function () {
+    const newer = JSON.parse(xhr.response); 
+    alert(newer.resp.text);
+  };
+  xhr.send(JSON.stringify({ text }));
+});
+
+
+document.addEventListener("selectionchange", () => {
+  if(textConfig.focusOffset < textConfig.anchorOffset){
+    text = textConfig.focusNode?.data.slice(textConfig.focusOffset, textConfig.anchorffset);
+  } else {
+    text = textConfig.focusNode?.data.slice(textConfig.anchorOffset, textConfig.focusOffset);
+  }
+})
